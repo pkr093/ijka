6013581256c8a4c7c9cc6508efb7f055e493da68
Pravin <100563576+pkr093@users.noreply.github.com>
2023-06-14 14:24:27 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 function gapSize(){
-    document.getElementById('home1').style.height='15vh';
+    document.getElementById('home1').style.height='14vh';
 }
+
+let burger=document.querySelector('.burger');
+let navbar=document.querySelector('.navbar');
+let navLeft=document.querySelector('.nav-left');
+
+
+burger.addEventListener('click',()=>{
+    navbar.classList.toggle('nav-height-res');
+    navLeft.classList.toggle('nav-vis-res');
+   
+})
